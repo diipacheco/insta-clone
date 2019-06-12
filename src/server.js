67d4dd8dfe98cb5b93116a06b1b04fd276576525
Edilson Pacheco <edilsopacheco6@gmail.com>
2019-06-12 16:01:18 -0300

    correction of the function cors and a insertion of a rote that allows a URL in every uploaded image

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 // Importação das dependências
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const path = require('path');
 
 // Objetos que fazem instancia das funções dessas dependências
 const app = express();
+
+const cors = require('cors');
+
+// habilitação para qualquer aplicação frontend acessar os protocolos da API
+app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -20,11 +25,11 @@ app.use((req, res, next) => {
   next();
 });
 
+// rota que dá acesso a uma url das imagens que foram feitas uploads
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
 // referência do arquivo de rotas
 app.use(require('./routes'));
-
-// habilitação para qualquer aplicação frontend acessar os protocolos da API
-app.use(cors);
 
 // porta padrão de desenvolvimento
 server.listen(3333);
